61df88e7ad75d144b3b1df3a5543923ac8ba0509
Alex <atol514@gmail.com>
2018-09-26 10:48:51 -0700

    a

---
+++ src/travelRoute.py
@@ -0,0 +1,11 @@
+class TravelRoute:
+    route = []
+    
+    def f(self, rep):
+        return 0
+
+    def setRoute(cities)
+        if(cities is list):
+            route = cities
+        else:
+            print('Error! Only acceptable input is a list!')
