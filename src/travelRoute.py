7c5f130770f78855dfe44210d301145d3ad49e3b
Alex Tol <atol514@gmail.com>
2018-09-26 18:41:38 -0700

    a

--- src/travelRoute.py
+++ src/travelRoute.py
@@ -1,11 +1,20 @@
+import random
+
 class TravelRoute:
     route = []
     
     def f(self, rep):
         return 0
 
-    def setRoute(cities)
+    def setRoute(cities):
         if(cities is list):
             route = cities
         else:
             print('Error! Only acceptable input is a list!')
+
+    def genRoute(cityList):
+        for city in cityList:
+            route.append(city)
+
+        random.shuffle(route)
+
