11e4828e67a7ef12f4302b72f3d6d3dc0c0730a0
Alex Tol <atol514@gmail.com>
2018-09-26 18:41:31 -0700

    a

---
+++ src/population.py
@@ -0,0 +1,10 @@
+class Population:
+    generations = 1
+    totalFitness = 0
+    individuals = []
+
+    def f(self, rep):
+        return 0
+
+    def addIndividual(i):
+        individuals.append(i)
